487ea296ba322276b8c78b1d053929de8dcbc3c2
bravian1 <nyatorobravian@gmail.com>
2024-05-09 10:53:38 +0300

    Added comments

--- average.go
+++ average.go
@@ -1,5 +1,8 @@
 package main
-
+/*
+function that calculates the average of a slice of float64
+by dividing the sum of all the numbers by the number of values
+*/
 func averageNum(n []float64) float64 {
 	sum := float64(0)
 	for i := 0; i < len(n); i++ {
